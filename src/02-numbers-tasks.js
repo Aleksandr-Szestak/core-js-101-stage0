ca2cb74de1d5ddcd6e82109c3d978205ad16f45a
Aleksandr Szestak <szestak@hotmail.com>
2025-09-07 23:31:42 +0300

    fix: remove eslint warning

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -50,7 +50,6 @@ function getCircleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  /* global BigInt */
   return Number((BigInt(value1) + BigInt(value2)) / BigInt(2));
 }
 
